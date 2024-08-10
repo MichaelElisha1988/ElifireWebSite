f91cbd6f3d759ecf5ebbcefd41c25c58d5abfd25
Michael Elisha <mishaelifire@gmail.com>
2024-08-10 10:56:23 +0300

    ..

--- src/app/routes/support-artist/support-artist.component.ts
+++ src/app/routes/support-artist/support-artist.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-support-artist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './support-artist.component.html',
-  styleUrl: './support-artist.component.scss'
+  styleUrl: './support-artist.component.scss',
 })
 export class SupportArtistComponent {
-
+  readonly isMobile = window.innerWidth < 640;
 }
